7ecba67936d8bba7e7cbea4f7f4fb0f6aa3fb2d2
Ansel Halliburton <ansel.halliburton@airbus-sv.com>
2018-02-28 12:18:24 -0800

    Disambiguate version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Bayesian',
-    version='0.1.7',
+    version='2018-02-28',
     author='Neville Newey',
     author_email='nn@theneweys.org',
     packages=['bayesian', 'bayesian.test', 'bayesian.examples',
